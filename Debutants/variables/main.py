e9be142d27731dc3aa41212d730793af76891d51
jonathan <jonathanmudiamabat@gmail.com>
2023-10-13 17:41:33 +0200

    mon prremier coomit

---
+++ Debutants/variables/main.py
@@ -0,0 +1,24 @@
+my_integer = 45
+my_string = 'jonathan'
+my_float = 34.56
+
+# print(my_float, my_string, my_integer) #45 jonathan 34.56
+
+age = 30
+age += 23  # 53
+
+
+# boolens
+
+a = 3
+b = 3
+# print(a == b) true
+
+multiplication = a*b
+addition = a+b
+soustraction = a-b
+division = a/b
+
+print(multiplication, division, soustraction, addition)
+
+# pour mettre en coomentaire
