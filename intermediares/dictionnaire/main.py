e040948f0586d2903414db0681cd0a6e418d10b2
Mudiambat13 <103556538+Mudiambat13@users.noreply.github.com>
2023-12-17 08:28:43 +0000

    Add dictionary folder

---
+++ intermediares/dictionnaire/main.py
@@ -0,0 +1,12 @@
+my_dict = {
+     "cle_1": "valeur_1",
+     "cle_2": "valeur_2",
+     "cle_3": "valeur_3",
+     "cle_4": "valeur_4",
+}
+print(my_dict)
+print(my_dict.keys())
+# print dictionary keys
+
+print(my_dict.values())
+# print dictionary values
